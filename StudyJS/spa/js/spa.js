67a426e4ff8f718633858cea9596df163d16f293
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-28 23:59:20 +0600

    setting up the application to use shell

--- StudyJS/spa/js/spa.js
+++ StudyJS/spa/js/spa.js
@@ -1,10 +1,6 @@
 var spa = (function () {
     var initModule = function ($container) {
-        $container.html(
-            '<h1 style="display:inline-block; margin: 25px;">'
-            + 'hello spa!'
-            + '</h1>'
-        );
+        spa.shell.initModule($container);
     };
     return {initModule: initModule};
 }());

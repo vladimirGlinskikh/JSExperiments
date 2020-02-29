6301bb718035d7278206d546d5ef1496971f36da
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-29 07:09:42 +0600

    processed the chat on opening and closing by mouse click

--- StudyJS/spa/js/spa.shell.js
+++ StudyJS/spa/js/spa.shell.js
@@ -18,11 +18,16 @@ spa.shell = (function () {
             chat_extend_time: 1000,
             chat_retract_time: 300,
             chat_extend_height: 450,
-            chat_retract_height: 15
+            chat_retract_height: 15,
+            chat_extended_title: 'Щелкните, чтобы свернуть',
+            chat_retracted_title: 'Щелкните, чтобы раскрыть'
         },
-        stateMap = { $container : null},
+        stateMap = {
+           $container : null,
+           is_chat_retracted: true
+        },
         jqueryMap = {},
-        setJqueryMap, toggleChat, initModule;
+        setJqueryMap, toggleChat, onClickChat, initModule;
 
     setJqueryMap = function () {
         var $container = stateMap.$container;
@@ -48,6 +53,10 @@ spa.shell = (function () {
                 {height : configMap.chat_extend_height},
                 configMap.chat_extend_time,
                 function () {
+                    jqueryMap.$chat.attr(
+                        'title', configMap.chat_extended_title
+                    );
+                    stateMap.is_chat_retracted = false;
                     if (callback){
                         callback(jqueryMap.$chat);
                     }
@@ -59,6 +68,10 @@ spa.shell = (function () {
             {height: configMap.chat_retract_height},
             configMap.chat_retract_time,
             function () {
+                jqueryMap.$chat.attr(
+                    'title', configMap.chat_retracted_title
+                );
+                stateMap.is_chat_retracted = true;
                 if (callback){
                     callback(jqueryMap.$chat);
                 }
@@ -67,17 +80,20 @@ spa.shell = (function () {
         return true;
     };
 
+    onClickChat = function(event){
+        toggleChat(stateMap.is_chat_retracted);
+        return false;
+    };
+
     initModule = function ($container) {
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
 
-        setTimeout(function () {
-            toggleChat(true);
-        }, 3000);
-        setTimeout(function () {
-            toggleChat(false);
-        }, 8000);
+        stateMap.is_chat_retracted = true;
+        jqueryMap.$chat
+            .attr('title', configMap.chat_retracted_title)
+            .click(onClickChat);
     };
     return {initModule : initModule};
 }());

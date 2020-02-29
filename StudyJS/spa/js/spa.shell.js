879171c570aec25ea61edff93aab1fe5bc52ab30
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-29 06:40:44 +0600

    funds added minimize and open chat windows

--- StudyJS/spa/js/spa.shell.js
+++ StudyJS/spa/js/spa.shell.js
@@ -13,21 +13,71 @@ spa.shell = (function () {
                 + '</div>'
                 + '<div class="spa-shell-foot"></div>'
                 + '<div class="spa-shell-chat"></div>'
-                + '<div class="spa-shell-modal"></div>'
+                + '<div class="spa-shell-modal"></div>',
+
+            chat_extend_time: 1000,
+            chat_retract_time: 300,
+            chat_extend_height: 450,
+            chat_retract_height: 15
         },
         stateMap = { $container : null},
         jqueryMap = {},
-        setJqueryMap, initModule;
+        setJqueryMap, toggleChat, initModule;
 
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = {$container: $container};
+
+        jqueryMap = {
+            $container : $container,
+            $chat : $container.find('.spa-shell-chat')
+        };
+    };
+
+    toggleChat = function(do_extend, callback){
+        var
+            px_chat_ht = jqueryMap.$chat.height(),
+            is_open = px_chat_ht === configMap.chat_extend_height,
+            is_closed = px_chat_ht === configMap.chat_retract_height,
+            is_sliding = ! is_open && ! is_closed;
+
+        if (is_sliding){
+            return false;
+        }
+        if (do_extend){
+            jqueryMap.$chat.animate(
+                {height : configMap.chat_extend_height},
+                configMap.chat_extend_time,
+                function () {
+                    if (callback){
+                        callback(jqueryMap.$chat);
+                    }
+                }
+            );
+            return true;
+        }
+        jqueryMap.$chat.animate(
+            {height: configMap.chat_retract_height},
+            configMap.chat_retract_time,
+            function () {
+                if (callback){
+                    callback(jqueryMap.$chat);
+                }
+            }
+        );
+        return true;
     };
 
     initModule = function ($container) {
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
+
+        setTimeout(function () {
+            toggleChat(true);
+        }, 3000);
+        setTimeout(function () {
+            toggleChat(false);
+        }, 8000);
     };
     return {initModule : initModule};
 }());

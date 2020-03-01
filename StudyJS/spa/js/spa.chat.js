3c8b9ef4dff5959e0a8fa770976139ec3b86b800
VladimirGlinskikh <support@zhelezyaka.kz>
2020-03-02 02:08:08 +0600

    create spa.chat.js file

---
+++ StudyJS/spa/js/spa.chat.js
@@ -0,0 +1,40 @@
+spa.chat = (function () {
+    var
+        configMap = {
+            main_html: String()
+                + '<div style="padding:1em; color:#fff;">'
+                + 'Say hello to chat'
+                + '</div>',
+
+            settable_map: {}
+        },
+
+        stateMap = {$container: null},
+        jqueryMap = {},
+        setJqueryMap, configModule, initModule;
+
+    setJqueryMap = function () {
+        var $container = stateMap.$container;
+        jqueryMap = {$container: $container};
+    };
+
+    configModule = function (input_map) {
+        spa.util.setConfigMap({
+            input_map: input_map,
+            settable_map: configMap.settable_map,
+            config_map: configMap
+        });
+        return true;
+    };
+
+    initModule = function ($container) {
+        $container.html(configMap.main_html);
+        stateMap.$container = $container;
+        setJqueryMap();
+        return true;
+    };
+    return {
+        configModule: configModule,
+        initModule: initModule
+    };
+}());

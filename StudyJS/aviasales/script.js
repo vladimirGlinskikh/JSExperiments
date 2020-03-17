a6b2b07529345b55aa649efba52106b1e13ed9a8
VladimirGlinskikh <support@zhelezyaka.kz>
2020-03-17 23:46:29 +0600

    write const formSearch

--- StudyJS/aviasales/script.js
+++ StudyJS/aviasales/script.js
@@ -1,1 +1,7 @@
-alert('test script');
+const formSearch = document.querySelector('.form-search'),
+    inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__dat-depart');
+

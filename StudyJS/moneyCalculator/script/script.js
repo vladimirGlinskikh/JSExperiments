107973260cb975e8fda74fe4d9244875c3ce079d
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-24 22:19:01 +0600

    creating constants

---
+++ StudyJS/moneyCalculator/script/script.js
@@ -0,0 +1,7 @@
+const totalBalance = document.querySelector('.total__balance'),
+    totalMoneyIncome = document.querySelector('.total__money-income'),
+    totalMoneyExpenses = document.querySelector('.total__money-expenses'),
+    historyList = document.querySelector('.history__list'),
+    form = document.getElementById('form'),
+    operationName = document.querySelector('.operation__name'),
+    operationAmount = document.querySelector('.operation__amount');

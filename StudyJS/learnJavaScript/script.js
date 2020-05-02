45b82683361172be1484317f45f0605a8352f700
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-03 00:06:49 +0600

    object

--- StudyJS/learnJavaScript/script.js
+++ StudyJS/learnJavaScript/script.js
@@ -1,2 +1,11 @@
-alert('hello world')
-console.log("hello friends");
+var object = {
+    a: 3,
+    b: 'JavaScript',
+    c: false,
+    d: {
+        e: 11,
+        f: 12
+    },
+    z: [1, 2, 3, 4]
+};
+console.log(object);

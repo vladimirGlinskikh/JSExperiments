0da5bbc2e749954fa570e70a021191c99559ce8f
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-27 00:33:59 +0600

    Open drop down menu with click on the icons

--- StudyJS/netclicks/script/main.js
+++ StudyJS/netclicks/script/main.js
@@ -5,3 +5,20 @@ hamburger.addEventListener('click', () => {
 	leftMenu.classList.toggle('openMenu');
 	hamburger.classList.toggle('open');
 });
+
+document.addEventListener('click', event => {
+	if (!event.target.closest('.left-menu')) {
+		leftMenu.classList.remove('openMenu');
+		hamburger.classList.remove('open');
+	}
+});
+
+leftMenu.addEventListener('click', event => {
+	const target = event.target;
+	const dropdown = target.closest('.dropdown');
+	if (dropdown) {
+		dropdown.classList.toggle('active');
+		leftMenu.classList.add('openMenu');
+		hamburger.classList.add('open');
+	}
+});

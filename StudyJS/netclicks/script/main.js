1235e612d59864c6028facd57fc61f16e71b3b78
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-27 19:57:03 +0600

    Change image

--- StudyJS/netclicks/script/main.js
+++ StudyJS/netclicks/script/main.js
@@ -44,3 +44,17 @@ modal.addEventListener('click', event => {
 		modal.classList.add('hide');
 	}
 });
+
+const changeImage = event => {
+	const card = event.target.closest('.tv-shows__item');
+
+	if (card) {
+		const img = card.querySelector('.tv-card__img');
+		if (img.dataset.backdrop) {
+			[img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+		}
+	}
+};
+
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);

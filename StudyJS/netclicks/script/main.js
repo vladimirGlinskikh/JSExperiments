bc4c3d24f7fb1df7b5c8c89381fb787e1cccc365
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-28 01:42:59 +0600

    Render card response

--- StudyJS/netclicks/script/main.js
+++ StudyJS/netclicks/script/main.js
@@ -1,7 +1,55 @@
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+
 const leftMenu = document.querySelector('.left-menu'),
 	hamburger = document.querySelector('.hamburger'),
 	tvShowsList = document.querySelector('.tv-shows__list'),
 	modal = document.querySelector('.modal');
+
+const DBService = class {
+	getData = async (url) => {
+		const res = await fetch(url);
+		if (res.ok) {
+			return res.json();
+		} else {
+			throw new Error(`don't have get value to ${url}`);
+		}
+	}
+	getTestData = () => {
+		return this.getData('test.json')
+	}
+}
+
+const renderCard = response => {
+	tvShowsList.textContent = '';
+
+	response.results.forEach(({
+								  backdrop_path: backdrop,
+								  name: title,
+								  poster_path: poster,
+								  vote_average: vote
+							  }) => {
+
+		const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+		const backdropIMG = '';
+		const voteElem = '';
+
+		const card = document.createElement('li');
+		card.className = 'tv-shows__item';
+		card.innerHTML = `
+		   <a href="#" class="tv-card">
+				<span class="tv-card__vote">${vote}</span>
+				<img class="tv-card__img"
+					src="${posterIMG}"
+					data-backdrop="${IMG_URL + backdrop}"
+					alt="${title}">
+				<h4 class="tv-card__head">${title}</h4>
+		   </a>
+		`;
+		tvShowsList.append(card);
+	});
+}
+
+new DBService().getTestData().then(renderCard);
 
 hamburger.addEventListener('click', () => {
 	leftMenu.classList.toggle('openMenu');

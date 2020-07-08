f59ff9cf38ad6c8831bfb794cadf8c96974981e7
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-08 12:17:22 +0600

    Open the modal window when you click on the "Submit Ad" button

--- StudyJS/avito/scripts/script.js
+++ StudyJS/avito/scripts/script.js
@@ -1,1 +1,9 @@
-alert('hi');
+'use strict';
+
+const modalAdd = document.querySelector('.modal__add'),
+	addAd = document.querySelector('.add__ad');
+
+
+addAd.addEventListener('click', () => {
+	modalAdd.classList.remove('hide');
+});

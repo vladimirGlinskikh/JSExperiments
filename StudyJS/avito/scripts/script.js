bfc3f4da0d1677375cd50b6ed85a7a77acb150b2
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-09 22:51:43 +0600

    Open and close the modal window by clicking on the "Test Card" card

--- StudyJS/avito/scripts/script.js
+++ StudyJS/avito/scripts/script.js
@@ -3,19 +3,31 @@
 const modalAdd = document.querySelector('.modal__add'),
 	addAd = document.querySelector('.add__ad'),
 	modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-	modalSubmit = document.querySelector('.modal__submit');
+	modalSubmit = document.querySelector('.modal__submit'),
+	catalog = document.querySelector('.catalog'),
+	modalItem = document.querySelector('.modal__item');
 
+const closeModal = function (event) {
+	const target = event.target;
+
+	if (target.closest('.modal__close') ||
+		target === this) {
+		this.classList.add('hide');
+		modalSubmit.reset();
+	}
+};
 
 addAd.addEventListener('click', () => {
 	modalAdd.classList.remove('hide');
 	modalBtnSubmit.disabled = true;
 });
 
-modalAdd.addEventListener('click', (event) => {
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+catalog.addEventListener('click', event => {
 	const target = event.target;
-	if (target.classList.contains('modal__close') ||
-		target === modalAdd) {
-		modalAdd.classList.add('hide');
-		modalSubmit.reset();
+	if (target.closest('.card')) {
+		modalItem.classList.remove('hide');
 	}
 });

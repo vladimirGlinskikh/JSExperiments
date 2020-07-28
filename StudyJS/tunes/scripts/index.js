e1477940ca1a9bb15855ad7dfd55126c2efaf57a
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-28 20:13:36 +0600

    Create and initialization js files

---
+++ StudyJS/tunes/scripts/index.js
@@ -0,0 +1,7 @@
+import {radioPlayerInit} from "./radioPlayer.js";
+import {musicPlayerInit} from "./musicPlayer.js";
+import {videoPlayerInit} from "./videoPlayer.js";
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();

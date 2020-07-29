afff5cbfe7de6025ea681e9c59cb7e362c63bc92
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-29 22:45:08 +0600

    Play the video and change the pause and start icon

--- StudyJS/tunes/scripts/videoPlayer.js
+++ StudyJS/tunes/scripts/videoPlayer.js
@@ -5,4 +5,23 @@ export const videoPlayerInit = () => {
 		videoTimePassed = document.querySelector('.video-time__passed'),
 		videoProgress = document.querySelector('.video-progress'),
 		videoTimeTotal = document.querySelector('.video-time__total');
+
+	const toggleIcon = () => {
+		if (videoPlayer.paused) {
+			videoButtonPlay.classList.remove('fa-pause');
+			videoButtonPlay.classList.add('fa-play');
+		} else {
+			videoButtonPlay.classList.add('fa-pause');
+			videoButtonPlay.classList.remove('fa-play');
+		}
+	}
+
+	videoPlayer.addEventListener('click', () => {
+		if (videoPlayer.paused) {
+			videoPlayer.play();
+		} else {
+			videoPlayer.pause();
+		}
+		toggleIcon();
+	});
 };

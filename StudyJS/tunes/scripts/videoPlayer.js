18ad4e4e808d25bccb8ac5a65fc482f6ea578339
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-29 22:31:21 +0600

    Create constants for video player

--- StudyJS/tunes/scripts/videoPlayer.js
+++ StudyJS/tunes/scripts/videoPlayer.js
@@ -1,3 +1,8 @@
 export const videoPlayerInit = () => {
-	console.log('Video Player Init');
+	const videoPlayer = document.querySelector('.video-player'),
+		videoButtonPlay = document.querySelector('.video-button__play'),
+		videoButtonStop = document.querySelector('.video-button__stop'),
+		videoTimePassed = document.querySelector('.video-time__passed'),
+		videoProgress = document.querySelector('.video-progress'),
+		videoTimeTotal = document.querySelector('.video-time__total');
 };

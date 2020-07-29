e28bab6cbabf20c0ef1916cec2f10c59e42215ad
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-30 03:24:59 +0600

    Create constants for radio player

--- StudyJS/tunes/scripts/radioPlayer.js
+++ StudyJS/tunes/scripts/radioPlayer.js
@@ -1,3 +1,10 @@
 export const radioPlayerInit = () => {
-	console.log('Radio Init');
+	const radio = document.querySelector('.radio'),
+		radioCoverImg = document.querySelector('.radio-cover__img'),
+		radioHeader = document.querySelector('.radio-header'),
+		radioNavigation = document.querySelector('.radio-navigation'),
+		radioItem = document.querySelectorAll('.radio-item'),
+		radioStop = document.querySelector('.radio-stop');
+
+	const audio = new Audio();
 };

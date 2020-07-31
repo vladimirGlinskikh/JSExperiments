c8f221a0ef2983e8510c36857e5fc71cc7943fb0
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-31 10:09:00 +0600

    Stop playback when switching between music, video and radio stations

--- StudyJS/tunes/scripts/index.js
+++ StudyJS/tunes/scripts/index.js
@@ -10,6 +10,10 @@ const deactivationPlayer = () => {
 	temp.style.display = 'none';
 	playerBtn.forEach(item => item.classList.remove('active'));
 	playerBlock.forEach(item => item.classList.remove('active'));
+
+	musicPlayerInit.stop();
+	videoPlayerInit.stop();
+	radioPlayerInit.stop();
 };
 
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
--- StudyJS/tunes/scripts/musicPlayer.js
+++ StudyJS/tunes/scripts/musicPlayer.js
@@ -103,4 +103,13 @@ export const musicPlayerInit = () => {
 		const progress = (x / allWidth) * audioPlayer.duration;
 		audioPlayer.currentTime = progress;
 	});
+
+	musicPlayerInit.stop = () => {
+		if (!audioPlayer.paused) {
+			audioPlayer.pause();
+			audio.classList.remove('play');
+			audioButtonPlay.classList.remove('fa-pause');
+			audioButtonPlay.classList.add('fa-play');
+		}
+	}
 };

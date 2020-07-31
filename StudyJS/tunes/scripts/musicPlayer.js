5c63034e7756bbe0a87ef739e875b59397fa8b76
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-31 07:50:05 +0600

    Create constants for the music player

--- StudyJS/tunes/scripts/musicPlayer.js
+++ StudyJS/tunes/scripts/musicPlayer.js
@@ -1,3 +1,12 @@
 export const musicPlayerInit = () => {
-	console.log('Music Player Init');
+	const audio = document.querySelector('.audio'),
+		audioImg = document.querySelector('.audio-img'),
+		audioHeader = document.querySelector('.audio-header'),
+		audioPlayer = document.querySelector('.audio-player'),
+		audioNavigation = document.querySelector('.audio-navigation'),
+		audioButtonPlay = document.querySelector('.audio-button__play'),
+		audioTimePassed = document.querySelector('.audio-time__passed'),
+		audioProgress = document.querySelector('.audio-progress'),
+		audioProgressTiming = document.querySelector('.audio-progress__timing'),
+		audioTimeTotal = document.querySelector('.audio-time__total');
 };

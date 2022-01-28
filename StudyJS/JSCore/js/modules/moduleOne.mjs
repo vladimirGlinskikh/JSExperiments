f0ead366b3051582eb3dc97c67224d65d612a81b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-01-28 21:16:35 +0600

    Modules in action

---
+++ StudyJS/JSCore/js/modules/moduleOne.mjs
@@ -0,0 +1,15 @@
+const myName = () => {
+    console.log('Vladimir')
+}
+
+const mult = (a, b) => a * b
+
+const one = 1
+const two = 'two'
+
+export {
+    myName,
+    mult,
+    one,
+    two
+}
---
+++ StudyJS/JSCore/js/modules/moduleTwo.mjs
@@ -0,0 +1,6 @@
+import {myName, mult, one, two} from './moduleOne.mjs'
+
+myName()
+console.log(mult(2,6))
+console.log(one)
+console.log(two)

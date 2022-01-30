5f32a866018e759edc71a62149b897721d1df973
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-01-30 22:32:09 +0600

    Promises in action

---
+++ StudyJS/JSCore/js/promises.js
@@ -0,0 +1,7 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })
+    .then(json => console.log(json))
+    .catch(error => console.error(error))
